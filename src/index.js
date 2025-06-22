672f4b1eec66af9a2e65e5536b6cb183bc8fcf35
ParthSinghPS <posiedon.1721@gmail.com>
2025-06-22 16:49:52 +0530

    error handling and api responses

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ dotenv.config({
     path: './env'
 })
 dbConnect()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at Port: ${process.env.PORT}`);
+        
+    })
+})
+.catch( (err) => {
+    console.log("MONGODB Connection Failed:" , err);
+    
+})
 
 /*
 import express from "express"
