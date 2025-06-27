0a377d78075904755eebfca5a6c0abbb4478d0d5
ParthSinghPS <posiedon.1721@gmail.com>
2025-06-27 15:19:47 +0530

    add getUserWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -376,6 +376,56 @@ const getUserChannelProfile = asyncHandler(async (req ,res) => {
     .json(new ApiResponse(200 , channel[0] , "Channel fetched successfully"))
 })
 
+const getUserWatchHistory = asyncHandler(async (req , res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200 , user[0].watchHistory , "watch history fetched successfullly"
+    ))
+})
+
 export {
     registerUser,
     loginUser,
@@ -386,5 +436,6 @@ export {
     UpdateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getUserWatchHistory
 }
